e50ed1f9a534b35be07db5d3c3004acba85105ae
Kyla Jang <kylajang@csu.fullerton.edu>
2021-02-22 20:19:51 -0800

    Adding first part "Yay it worked!"

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Kyla Jang
+CWID: 888078243
+Email: kylajang@csu.fullerton.edu
+*/
+
+
+// Directives interpreted by the preprocessor
+#include <iostream> 
+
+using std::cout;
+using std::endl;
+
+int main ()
+{
+	cout << "Yay it worked!";
+	return 0;
+}
+
