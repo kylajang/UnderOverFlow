43012710211242c7724f10c9e0b391c3926b066f
Kyla Jang <kylajang@csu.fullerton.edu>
2021-02-24 17:46:39 -0800

    Adding the constants program

---
+++ constants.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main ()
+{
+// Declaring my variables
+  const int a = 0;
+
+// Declaring the constant to a different value
+  a = 1;
+
+  return 0;
+}
